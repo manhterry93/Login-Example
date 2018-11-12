20a5d75eb8483813517ef330afa08d317833c155
hitagi007 <manhterry93@gmail.com>
2018-11-13 00:35:29 +0700

    Add Zalo Sdk

--- app/src/main/java/itto/pl/loginexample/MainActivity.java
+++ app/src/main/java/itto/pl/loginexample/MainActivity.java
@@ -19,26 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("PL_itto","hash : "+getApplicationHashKey(this));
     }
 
-
-    static String getApplicationHashKey(Context ctx) {
-        try {
-            PackageInfo info = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String sig = Base64.encodeToString(md.digest(), Base64.DEFAULT).trim();
-                if (sig.trim().length() > 0) {
-                    return sig;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
-        return "";
-    }
 }
